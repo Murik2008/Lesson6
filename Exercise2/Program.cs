169a032401f4b0ec3e39776287f608db2e35f706
Murad <baghmr24@gmail.com>
2023-12-17 19:15:34 +0400

    Exercise 2

--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -1,2 +1,7 @@
 ﻿//Задайте строку, содержащую латинские буквы в обоих регистрах. 
 //Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+string str = "Hello, my Dear Santa!";
+string str1 = str.ToLower();
+Console.WriteLine(str);
+Console.WriteLine();
+Console.WriteLine(str1);
