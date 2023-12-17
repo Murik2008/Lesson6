bb73c4e7eaa44c3da346ad25fe422a46c4cc640e
Murad <baghmr24@gmail.com>
2023-12-17 19:33:32 +0400

    Exercise 3

---
+++ Exercise3/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задайте произвольную строку. Выясните, является ли она палиндромом.
+Console.WriteLine("Задайте произвольную строку:");
+string str = Console.ReadLine();
+void StringIsPalindrom(string str)
+{
+    for (int i = 0; i < str.Length/2; i++)
+    {
+        if (str[i] == str[str.Length-i-1])
+        {
+            Console.WriteLine("Да, это строка является полиндромом.");
+            break;
+        }
+        else 
+        {
+            Console.WriteLine("Нет, это строка не является полиндромом.");
+            break;
+        }
+    }
+}
+Console.WriteLine();
+StringIsPalindrom(str);
